1cb4aca50e2a2f5551855134e71f57cecdcd6ae7
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-09-26 03:03:45 +0530

    refactor add api

--- block-BNaaeh/Bookstore/app.js
+++ block-BNaaeh/Bookstore/app.js
@@ -3,10 +3,20 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var mongoose = require("mongoose");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var bookRouter = require("./routes/book");
+
+//connect to mongo
+mongoose.connect(
+  "mongodb://localhost/bookstore",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    console.log(err ? err : "Connected true");
+  }
+);
 
 var app = express();
 
@@ -21,8 +31,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
 app.use("/book", bookRouter);
+app.use("/users", usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
