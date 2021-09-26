bd93311aec1324cd9cc846a45a28527ee9e0624e
ADITYA NARAYAN SINGH <3daditya@gmail.com>
2021-09-26 11:46:45 +0530

    complted

--- block-BNaaeh/Bookstore/app.js
+++ block-BNaaeh/Bookstore/app.js
@@ -8,6 +8,7 @@ var mongoose = require("mongoose");
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var bookRouter = require("./routes/book");
+var commentRouter = require("./routes/comment");
 
 //connect to mongo
 mongoose.connect(
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/book", bookRouter);
 app.use("/users", usersRouter);
+app.use("/comment", commentRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
